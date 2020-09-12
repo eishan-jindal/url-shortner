389f30987d69577aee16b09e8427e0c876dccea7
eishan-jindal <eishanjindal88@gmail.com>
2020-09-12 13:26:53 +0530

    changes

---
+++ src/main/java/com/zen/urlShortner/controller/UrlController.java
@@ -0,0 +1,24 @@
+package com.zen.urlShortner.controller;
+
+import com.zen.urlShortner.service.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("urlMapper")
+public class UrlController {
+
+    private final UrlService urlService;
+
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
+    @GetMapping("/get")
+    public String printGreeting() {
+
+        return urlService.getMessage();
+    }
+}
---
+++ src/main/java/com/zen/urlShortner/service/UrlService.java
@@ -0,0 +1,11 @@
+package com.zen.urlShortner.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UrlService {
+    public String getMessage(){
+        String message = "hello";
+        return message;
+    }
+}
